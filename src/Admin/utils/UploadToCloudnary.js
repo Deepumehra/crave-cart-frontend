b317d5dea52e01562b3b23fecbdbe2555441dbec
Deepumehra <142345479+Deepumehra@users.noreply.github.com>
2024-06-28 12:39:04 +0530

    Update UploadToCloudnary.js

--- src/Admin/utils/UploadToCloudnary.js
+++ src/Admin/utils/UploadToCloudnary.js
@@ -1,22 +1,29 @@
 export const uploadToCloudinary = async (pics) => {
-    
-    if (pics) {
-      const data = new FormData();
-      data.append("file", pics);
-      data.append("upload_preset", "Crave-cart");
-      data.append("cloud_name", " dpksujnt8");
-  
-      const res = await 
-      fetch(`https://api.cloudinary.com/v1_/dpksujnt8/image/upload`, {
-        method: "post",
+  if (pics) {
+    const data = new FormData();
+    data.append("file", pics);
+    data.append("upload_preset", "Crave-cart");
+    data.append("cloud_name", "dpksujnt8");
+
+    try {
+      const res = await fetch(`https://api.cloudinary.com/v1_1/dpksujnt8/image/upload`, {
+        method: "POST",
         body: data,
-      })
-        
-        const fileData=await res.json();
-        console.log("url : ", fileData);
-        return fileData.url
-  
-    } else {
-      console.log("error");
+      });
+
+      if (!res.ok) {
+        throw new Error(`Error: ${res.statusText}`);
+      }
+
+      const fileData = await res.json();
+      console.log("url:", fileData.url);
+      return fileData.url;
+    } catch (error) {
+      console.error("Upload error:", error);
+      return null;
     }
-  };
+  } else {
+    console.log("No file provided for upload.");
+    return null;
+  }
+};
